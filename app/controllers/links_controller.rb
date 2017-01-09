d3483fa508c4c1087f082e5299c00dee3f5178fd
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 10:24:43 -0700

    Complete edit link functionality

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,6 +10,18 @@ class LinksController < ApplicationController
   end
 
   def update
-    
+    @link = Link.find(params[:id])
+    if @link.update(link_params)
+      flash[:success] = "Link updated"
+      redirect_to links_path
+    else
+      flash.now[:error] = @link.errors.full_messages.join(", ")
+      render "edit"
+    end
   end
+
+  private
+    def link_params
+      params.require(:link).permit(:title, :url)
+    end
 end
--- spec/features/edit_link_spec.rb
+++ spec/features/edit_link_spec.rb
@@ -21,4 +21,17 @@ describe "edit link" do
     expect(page).to_not have_text(link.title)
     expect(page).to_not have_text(link.url)
   end
+
+  it "fails edit with bad url" do
+    link = create(:link)
+
+    visit links_path
+
+    click_on "Edit"
+    fill_in "link_title", with: "A thing"
+    fill_in "link_url", with: "google.com"
+    click_on "Update Link"
+
+    expect(page).to have_text("Url is not a valid URL")
+  end
 end
