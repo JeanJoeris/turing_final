095847fd0feee87781dd08c0abefacf4ce0f5a32
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 13:04:25 -0700

    Add functionality for mark as read creating a read entry

--- app/assets/javascripts/mark-read.js
+++ app/assets/javascripts/mark-read.js
@@ -5,11 +5,28 @@ $(document).ready(function(){
   $('#links-list').on('click', 'button.mark-read', function(){
     var $this = $(this);
     var linkId = $this.parents('.link').data('id');
-    
+
     $.ajax({
       url: '/api/v1/links/' + linkId,
       method: 'PATCH',
       data: {read: true}
-    });
+    })
+    .then( createReadEntry )
+    .then( updateLink );
   })
 })
+
+function createReadEntry(link) {
+  linkId = link.id
+
+  $.ajax({
+    url: "http://localhost:4000/api/v1/read",
+    method: 'POST',
+    data: {link_id: linkId},
+    type: 'json'
+  })
+}
+
+function updateLink(link) {
+
+}
