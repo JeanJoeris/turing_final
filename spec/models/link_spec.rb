175948fd9d723abb5e9ee3a0c36216c9e8129136
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 15:30:24 -0700

    Add sorted links by user method with test

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Link, type: :model do
     expect(link_1.valid?).to eq(false)
     expect(link_2.valid?).to eq(false)
   end
+
+  it "returns ordered links by user" do
+    Link.create(url: "http://xkcd.com", title: "a great webcomic", user_id: 1)
+    Link.create(url: "http://penny-arcade.com", title: "a good webcomic", user_id: 1)
+    Link.create(url: "http://penny-arcade.com", title: "a good webcomic", user_id: 2)
+    ordered_links = Link.by_user_id(1)
+
+    expect(ordered_links.count).to eq(2)
+    # expect to return in _reverse_ of creation order
+    expect(ordered_links.first.title).to eq("a good webcomic")
+    expect(ordered_links.last.title).to eq("a great webcomic")
+  end
 end
