f32e25304dd672361d9d0650d7978291f27aca80
Jean Joeris <joeris.peter@gmail.com>
2017-01-09 11:47:44 -0700

    Fix edit link after adding user_id to link

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LinksController < ApplicationController
 
   def create
-    @link = Link.new link_params
+    @link = Link.new link_params.merge(user_id: current_user.id)
     if @link.save
       render json: @link, status: 201
     else
--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -2,5 +2,6 @@ FactoryGirl.define do
   factory :link do
     url "http://xkcd.com"
     title "A great webcomic"
+    user
   end
 end
